ebda2214f04d52984079a989a9a99594d4d9b844
unknown <luisstalivieri29@gmail.com>
2021-09-27 21:45:53 -0300

    fui resolvido o problema de consumo de bateria

--- App.js
+++ App.js
@@ -73,12 +73,12 @@ export default function App() {
 
         {buttons.map((button) => 
           button === '=' ? // Mapeamento do botão =
-        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#3dd0e3'}]}>
+        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#350c56'}]}>
           <Text style={[styles.textButton, {color: "white", fontSize: 30}]}>{button}</Text>
         </TouchableOpacity>
           : // Mapeamento dos outros botões
           <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
-            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'black': '#0093a6'}]}>{button}</Text>
+            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'white': '#a2a2a2'}]}>{button}</Text>
           </TouchableOpacity>
         )}
       </View>
@@ -94,7 +94,7 @@ const styles = StyleSheet.create({
   results: {
     flex: 2,
     justifyContent: "center",
-    backgroundColor: "#f5f5f5"
+    backgroundColor: "#2a0d41"
   },
   resultText: {
     color: "#282F38",
@@ -112,17 +112,18 @@ const styles = StyleSheet.create({
   buttons: {
     flexDirection: 'row',
     flexWrap: 'wrap',
+    backgroundColor: '#4b0082',
   },
   button: {
-    backgroundColor: 'white',
+    backgroundColor: '#4b0082',
     alignItems: 'center',
     justifyContent: 'center',
-    minWidth: 90, 
-    minHeight: 90,
+    minWidth: 101, 
+    minHeight: 101,
     flex: 2,
   },
   textButton: {
-    color: "#7c7c7c",
+    color: "#ffffff",
     fontSize: 20,
   } 
 });
